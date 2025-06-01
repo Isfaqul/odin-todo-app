5e4433e0eb89c48bb3fe6865c6437b2e35567dab
Isfaqul <isfaq.xperia@gmail.com>
2025-06-01 12:11:08 +0530

    Remove unncessary code

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const postcssPresetEnv = require("postcss-preset-env");
 
 module.exports = {
   mode: "production",
